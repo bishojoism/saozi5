d21e1f8132bd6d6460a8a08e177440551b5a9cd6
bishojoism <bishojoism@163.com>
2025-05-25 19:33:39 +0800

    修复构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import wasm from "vite-plugin-wasm"
-import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     wasm(),
-    legacy({
-      targets: ['defaults'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-    }),
     react()
-  ]
+  ],
+  build: {
+    target: 'esnext'
+  }
 })
