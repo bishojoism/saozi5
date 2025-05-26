49472e3f965037bb8fcaabcd339eb5c6c5752582
bishojoism <bishojoism@163.com>
2025-05-26 14:36:05 +0800

    v5.4

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ export default function App() {
       <AppBar>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            图片臊子 5.3
+            图片臊子 5.4
           </Typography>
           <IconButton href="https://github.com/bishojoism/saozi5" color="inherit" size="large" edge="end">
             <GitHub />
@@ -41,6 +41,9 @@ export default function App() {
         <Stack spacing={2}>
           <Typography>
             第五代图片混淆技术，牺牲少许视觉效果获得抗等比缩放能力（现在不用点保存原图了）。
+          </Typography>
+          <Typography>
+            v5.4：重新兼容老旧设备。
           </Typography>
           <Typography>
             v5.3：放弃兼容老旧设备，压缩处理后的图片，新增代号功能：将图片传至图床（推荐<Link href="https://catbox.moe">Catbox</Link>，链接加上前缀<code>https://images.weserv.nl/?url=</code>即可代理图片令国内可访问），获取到的链接用<Link href="https://yinzhe9.netlify.app">喵喵隐者9</Link>加密，即可生成代号。
