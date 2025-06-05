25a753aa2449baf90be61628b562b44fb674b633
bishojoism <bishojoism@163.com>
2025-06-06 00:54:53 +0800

    sleep 1000

--- src/App.tsx
+++ src/App.tsx
@@ -268,7 +268,10 @@ function EverythingCode() {
                   }
                   else if (code.startsWith('油')) files = await de(code.substring(1))
                   else throw new Error('不支持这种格式的代号')
-                  for (let file of (files as File[])) saveAs(file, file.name)
+                  for (let file of files) {
+                    saveAs(file, file.name)
+                    await new Promise(resolve => setTimeout(resolve, 1000))
+                  }
                   setLoading(false)
                 } catch (e) {
                   setLoading(false)
